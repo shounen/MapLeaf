42bbfebcddcef7b5a4418d7c7a5547142e2cf544
nawedume <umar.nawed@mail.utoronto.ca>
2016-11-03 10:09:18 -0400

    Removed old csv method

--- WebApplication3/Default.aspx.cs
+++ WebApplication3/Default.aspx.cs
@@ -21,29 +21,7 @@ namespace WebApplication3
 
         }
 
-        [WebMethod]
-        public static string getJSON()
-        {
-            string jsonString = "";
-            // Read in file from a server side csv file. 
-            String[] values = File.ReadAllText(@"c:\users\umer\documents\visual studio 2015\Projects\WebApplication3\WebApplication3\EmployeeData.csv").Split(',', '\n');
-            int i = values.Length / 5;
-            Person[] Employees = new Person[i - 1];
-            // Make list of employees and add their data from the csv file.
-            for (int k = 1; k < i; ++k)
-            {
-                string first = values[k * 5 + 1];
-                string last = values[k * 5];
-                string preferred = values[k * 5 + 2];
-                string position = values[k * 5 + 3];
-                string location = values[k * 5 + 4];
-                Person Person1 = new Person(first, last, preferred, position, location);
-                Employees[k - 1] = Person1;
-            }
-            var javaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-            jsonString = javaScriptSerializer.Serialize(Employees);
-            return jsonString;
-        }
+        
         [WebMethod]
         public static string querySQL(string query, string column)
         {
